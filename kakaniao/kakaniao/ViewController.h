e352d58523fe522bda2de78123eddb1933dcde1c
dongchao <dongchao@kakaniao.com>
2015-05-13 10:33:57 +0800

    no message

---
+++ kakaniao/kakaniao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kakaniao
+//
+//  Created by dongchao on 15/5/13.
+//  Copyright (c) 2015年 dongchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ kakaniao/kakaniao/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kakaniao
+//
+//  Created by dongchao on 15/5/13.
+//  Copyright (c) 2015年 dongchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
